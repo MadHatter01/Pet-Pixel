c1ec6990dffdbf431c4ae39497283e4072ae8409
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-12-28 23:31:03 -0800

    circular motion and fixes to random motion

--- src/extension.ts
+++ src/extension.ts
@@ -52,33 +52,39 @@ class PetPixelViewProvider implements vscode.WebviewViewProvider {
                     const ctx = canvas.getContext('2d');
                    let x = 150;
                    let y = 150;
+                   let mousex = x;
+                   let mousey = y;
                    let radius = 10;
                    let dx = (Math.random() - 0.5)*4;
                    let dy = (Math.random() - 0.5)*4;
                    let isFollowing = false;
                    let mouseTimeout;
-
+                    let angle = 0;
+                    let isHovering = false;
                    canvas.addEventListener('mousemove', (event)=>{
                         const rect = canvas.getBoundingClientRect();
-                        const mousex = event.clientX - rect.left;
-                        const mousey = event.clientY - rect.top;
-                        x = mousex;
-                        y = mousey;
+                         mousex = event.clientX - rect.left;
+                         mousey = event.clientY - rect.top;
+                        isHovering = true;
                         isFollowing = true;
+                   });
 
-                         clearTimeout(mouseTimeout);
-        mouseTimeout = setTimeout(() => {
-            isFollowing = false;
-        }, 3000);
+                   canvas.addEventListener('mouseleave', ()=>{
+                   isHovering = false;
+                   
                    })
                     function animate(){
                         ctx.clearRect(0, 0, canvas.width, canvas.height);
-                        ctx.fillStyle = 'white';
-                        ctx.beginPath();
-                        ctx.arc(x, y, 10, 0, Math.PI * 2);
-                        ctx.fill();
-
+                    
+                    if (isHovering) {
+                       
+                            x = mousex + 30 * Math.cos(angle);
+                            y = mousey + 30 * Math.sin(angle);
+                            angle += 0.05; 
+                        }
+                    else{
                         x += dx ;
+                        y+= dy;
 
                         if(x + radius > canvas.width || x - radius < 0){
                             dx = -dx
@@ -87,7 +93,11 @@ class PetPixelViewProvider implements vscode.WebviewViewProvider {
                         if(y + radius > canvas.height || y - radius < 0){
                             dy = -dy;
                         }
-
+    }
+                        ctx.fillStyle = 'white';
+                        ctx.beginPath();
+                        ctx.arc(x, y, 10, 0, Math.PI * 2);
+                        ctx.fill();
                         requestAnimationFrame(animate);
                     }
 
