e673ef1fac1bb4d8dc6b4e7d1e2908056184f8cc
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-12-29 22:00:53 -0800

    move js functionality out - media folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
     const petPixelViewProvider = new PetPixelViewProvider(context);
@@ -24,6 +26,11 @@ class PetPixelViewProvider implements vscode.WebviewViewProvider {
         token: vscode.CancellationToken
     ) {
         webviewView.webview.options = { enableScripts: true };
+
+        const scriptPath = vscode.Uri.file(
+            path.join(this._context.extensionPath, 'media', 'script.js')
+        );
+        const scriptUri = webviewView.webview.asWebviewUri(scriptPath);
         webviewView.webview.html = `
             <!DOCTYPE html>
             <html>
@@ -45,64 +52,9 @@ class PetPixelViewProvider implements vscode.WebviewViewProvider {
                 </style>
             </head>
             <body>
-                <canvas id="petCanvas" width="150" height="150"></canvas>
-                <script>
+            <canvas id="petCanvas" width="150" height="150"></canvas>
+                            <script src="${scriptUri}"></script>
 
-                    const canvas = document.getElementById('petCanvas');
-                    const ctx = canvas.getContext('2d');
-                   let x = 150;
-                   let y = 150;
-                   let mousex = x;
-                   let mousey = y;
-                   let radius = 10;
-                   let dx = (Math.random() - 0.5)*4;
-                   let dy = (Math.random() - 0.5)*4;
-                   let isFollowing = false;
-                   let mouseTimeout;
-                    let angle = 0;
-                    let isHovering = false;
-                   canvas.addEventListener('mousemove', (event)=>{
-                        const rect = canvas.getBoundingClientRect();
-                         mousex = event.clientX - rect.left;
-                         mousey = event.clientY - rect.top;
-                        isHovering = true;
-                        isFollowing = true;
-                   });
-
-                   canvas.addEventListener('mouseleave', ()=>{
-                   isHovering = false;
-                   
-                   })
-                    function animate(){
-                        ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    
-                    if (isHovering) {
-                       
-                            x = mousex + 30 * Math.cos(angle);
-                            y = mousey + 30 * Math.sin(angle);
-                            angle += 0.05; 
-                        }
-                    else{
-                        x += dx ;
-                        y+= dy;
-
-                        if(x + radius > canvas.width || x - radius < 0){
-                            dx = -dx
-                        }
-
-                        if(y + radius > canvas.height || y - radius < 0){
-                            dy = -dy;
-                        }
-    }
-                        ctx.fillStyle = 'white';
-                        ctx.beginPath();
-                        ctx.arc(x, y, 10, 0, Math.PI * 2);
-                        ctx.fill();
-                        requestAnimationFrame(animate);
-                    }
-
-                    animate();
-                </script>
             </body>
             </html>
         `;
